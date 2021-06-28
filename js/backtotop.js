2222084e71dee3ccf7539bcc7974b79c142c1afd
lukin77 <josip.lukin7@gmail.com>
2021-06-28 21:17:28 +0200

    Jos malo pa gotovo

---
+++ js/backtotop.js
@@ -0,0 +1,32 @@
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+if (window.pageYOffset == 0) {
+  backToTopButton.style.display = "none";
+}
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) {
+    if (!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  }
+  else {
+    if (backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function () {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+function smoothScrollBackToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
